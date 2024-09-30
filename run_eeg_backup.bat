881605bd5efda7c9f1ba5a519389b7f997a68d17
jackliuyc <46500003+jackliuyc@users.noreply.github.com>
2024-09-30 15:28:13 -0400

    Update run_eeg_backup.bat

--- run_eeg_backup.bat
+++ run_eeg_backup.bat
@@ -1,17 +1,19 @@
 @echo off
+echo Starting batch script...
 
 REM Change directory to project folder
+echo Changing working directory...
 cd C:\Users\Public\Documents\GitHub\filemanagement-post
 
 REM Activate virtual env
+echo Activating virtual environment...
 call C:\Users\Public\Documents\PythonEnvironments\eeg_backup_env\Scripts\activate
 
 REM Run app
+echo Running python script...
 python eeg_backup.py
 
-REM Deactivate virtual environment after complete
-deactivate
+echo Batch script completed.
 
 REM Pause to keep window open
 pause
-
